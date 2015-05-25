9cd87a54de3bc1ae1065bcda163ea6544f7315c8
paa3 <paa3@users.noreply.github.com>
2015-05-25 21:45:43 +0200

    Removed README content from source code.

--- seqraii.h
+++ seqraii.h
@@ -1,44 +1,9 @@
 ﻿/**
- * Sequential resource allocation and initialization is commonly encountered when working with C provided APIs.
- * In general multiple steps must be taken when aquiring and preparing a resource for use. A common usecase would
- * be to create e.g. an UDP socket for communication:
- *   First the socket must be created, then we may need to configure it using ioctl or other API calls. Perhaps
- *   we must also do a server name look-up too, or memory must be allocated for a buffer before we are ready to
- *   use the socket.
- * All of these steps in preparing the resource must be done in a more or less strict order. They can also fail at
- * any point and we must be ready to perform a full cleanup of the preceeding steps to avoid locking up any resources.
- *
- * Ultimately we want to check for success or failure at each step and upon failure we want to undo all the previous steps
- * in the reverse order. When we're done using the resource, cleanup should be run as well. Common implementations
- * often result in a convoluted, error-prone approach. Enter SequentialRaii.
- *
- * Through extensive use of lambda expressions it offers the following advantages:
- * - All the code for initializing and cleaning up a resource is localized in space and time. The code is also specified
- *   only once. This results in increased maintainability and significantly reduced possibility for bugs.
- * - Offers automatic cleanup in correct inverse order.
- *
- * Lambda expressions aren't without a disadvantage:
- * - As with any lambda use special care must be taken to manage the lifetime of any variables captured by reference.
- *   For example, the following code will generate a runtime exception since c's destructor is run before seqraii's
- *   when leaving the scope:
- *     {
- *       SequentialRaii seqraii;
- *       std::vector<int> c;
- *       seqraii.addStep([&](){c.push_back(1); return true;}, [&](){c.pop_back();});
- *       seqraii.initialize();
- *     }
- *   The modified code below will run just fine within the limits defined by C++:
- *     {
- *       std::vector<int> c;    // Swapped this line with the one below
- *       SequentialRaii seqraii;
- *       seqraii.addStep([&](){c.push_back(1); return true;}, [&](){c.pop_back();});
- *       seqraii.initialize();
- *     }
- *
- * Other information:
- *   Copyright 2015 Torjus Breisjøberg.
- *   License: MIT license. See separate file for full disclaimer.
- *   Compile with C++14 enabled.
+ * Sequential resource allocation and initialization.
+ * 
+ * Copyright 2015 Torjus Breisjøberg.
+ * License: MIT license. See separate file for full disclaimer.
+ * Compile with C++14 enabled.
  */
 #pragma once
 
